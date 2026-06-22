77f3c5c9b5d06b862d68232e9f4b1b69dcc5bffe
Abirami Shanmugam <shanmugamarigaran@gmail.com>
2026-06-22 09:31:52 +0530

    Update api.js

--- task-management-app/frontend/src/api.js
+++ task-management-app/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://taskflow-app-1-4yzc.onrender.com",
+  baseURL: "https://taskflow-app-1-4yzc.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
